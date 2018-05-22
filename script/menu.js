53641f39ca9a89d30808a6bc9fd8b75cdc5b0ebe
linus97johansson <linus97johansson@gmail.com>
2018-05-22 13:24:29 +0200

    changes
    fekke

--- script/menu.js
+++ script/menu.js
@@ -1,20 +1,3 @@
-
-$.ajax({
-    url: "html/menu.html",
-    dataType: "html"
-}).done(function(data) {
-
-   data = "<div>"+data+"</div>";
-   $list = $(data).find("#menu");
-   console.log($list);
-
-   $eLearning = $(data).find("#e_learning");
-   console.log($eLearning);
-
-});
-
-$("#education").focus(function(){
-
 console.log("focused");
 $("#education").after($list);
 
