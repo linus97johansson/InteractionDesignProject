e4a3371eb4923e75fa47445aa714f152f0dde5e1
Max Timje <max@timje.se>
2018-05-22 12:08:49 +0200

    Include the HTML to the body

--- script/login.js
+++ script/login.js
@@ -1,5 +1,23 @@
 $(document).ready(function ($) {
 
-
+	$.ajax({
+		url: './html/login.html',
+		dataType: 'html',
+		
+	})
+	.done(function(data) {
+		console.log("success");
+		var elem = document.createElement('div');
+		elem.innerHTML = data;
+		document.body.append(elem);
+		debugger;
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	
 
 });
